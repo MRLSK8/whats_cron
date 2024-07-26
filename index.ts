f4f363850aaa515d7f4b23b768b0ba9590e070f5
Marcelo Lima <jose.lima@compasso.com.br>
2024-07-26 14:48:55 -0300

    change

--- index.ts
+++ index.ts
@@ -21,8 +21,11 @@ app
   })
   .then(() => {
     console.log("running server");
+
     const Grupo_test = process.env.USER_ID;
-    console.log("Grupo_test", { Grupo_test });
+    const Grupo_test_2 = process.env.USER_ID_2;
+
+    console.log("Grupo_test", { Grupo_test, Grupo_test_2 });
 
     client.on("qr", (qr) => {
       console.log("should read qr code");
@@ -31,11 +34,24 @@ app
 
     client.on("ready", async () => {
       console.log("Client is ready!");
+
       cron.schedule(
-        "7 14 * * 1-5",
+        "45 8 * * *",
         () => {
-          console.log("sending message");
-          client.sendMessage(Grupo_test, "Testando mensagem");
+          console.log("sending message group 1");
+          client.sendMessage(Grupo_test, "Bom dia 😊");
+        },
+        {
+          scheduled: true,
+          timezone: "America/Sao_Paulo",
+        }
+      );
+
+      cron.schedule(
+        "45 8 * * 1",
+        () => {
+          console.log("sending message group 2");
+          client.sendMessage(Grupo_test_2, "Eu vou 🙋🏽‍♂️");
         },
         {
           scheduled: true,
