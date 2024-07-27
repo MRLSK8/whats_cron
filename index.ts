c8d4294bd5a55bc4fad1b5c769907d05d65c2c60
Marcelo Lima <jose.lima@compasso.com.br>
2024-07-27 14:43:22 -0300

    change

--- index.ts
+++ index.ts
@@ -23,23 +23,30 @@ app
     console.log("running server");
 
     const Grupo_test = process.env.USER_ID;
+    console.log("Grupo_test", { Grupo_test });
     const Grupo_test_2 = process.env.USER_ID_2;
-
-    console.log("Grupo_test", { Grupo_test, Grupo_test_2 });
 
     client.on("qr", (qr) => {
       console.log("should read qr code");
       qrcode.generate(qr, { small: true });
     });
 
+    client.on("message", async (msg) => {
+      const chat = await msg.getChat();
+      
+      console.log({ msg, chat });
+
+      await chat.sendMessage("Hello :)");
+    });
+
     client.on("ready", async () => {
       console.log("Client is ready!");
 
       cron.schedule(
-        "45 8 * * *",
+        "50 14 * * *",
         () => {
           console.log("sending message group 1");
-          client.sendMessage(Grupo_test, "Bom dia 😊");
+          client.sendMessage(Grupo_test, "Como vc tá?");
         },
         {
           scheduled: true,
@@ -47,17 +54,17 @@ app
         }
       );
 
-      cron.schedule(
-        "45 8 * * 1",
-        () => {
-          console.log("sending message group 2");
-          client.sendMessage(Grupo_test_2, "Eu vou 🙋🏽‍♂️");
-        },
-        {
-          scheduled: true,
-          timezone: "America/Sao_Paulo",
-        }
-      );
+      // cron.schedule(
+      //   "45 8 * * 1",
+      //   () => {
+      //     console.log("sending message group 2");
+      //     client.sendMessage(Grupo_test_2, "Eu vou 🙋🏽‍♂️");
+      //   },
+      //   {
+      //     scheduled: true,
+      //     timezone: "America/Sao_Paulo",
+      //   }
+      // );
     });
 
     client.initialize();
